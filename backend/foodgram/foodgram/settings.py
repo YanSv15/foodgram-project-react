76f24a57468c5662f78b7f6980962e099e99dd49
Ян Свиридов <lansviridovlansviridov@yandex.ru>
2023-12-01 13:01:07 +0300

    правки закончил

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -80,12 +80,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
 
 DATABASES = {
     'default': {
@@ -158,6 +152,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/data'),
+]
 
 
 DJOSER = {
--- backend/foodgram/posts/management/commands/import_csv.py
+++ backend/foodgram/posts/management/commands/import_csv.py
@@ -1,25 +1,36 @@
 import csv
 import os
-
+import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from posts.models import Ingredient
 
-
-def get_reader(file_name: str):
-    csv_path = os.path.join(settings.BASE_DIR, 'static/data/', file_name)
-    csv_file = open(csv_path, 'r', encoding='utf-8')
-    reader = csv.reader(csv_file, delimiter=',')
-    return reader
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        csv_reader = get_reader('ingredients.csv')
-        for row in csv_reader:
-            obj, created = Ingredient.objects.get_or_create(
-                name=row[0],
-                measurement_unit=row[1]
-            )
-        print(f'ingredients - OK')
+        static_path = settings.STATICFILES_DIRS[0]
+        csv_file_path = os.path.join(static_path, 'data', 'ingredients.csv')
+
+        try:
+            with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                count_created = 0
+
+                for name, measurement_unit in csv_reader:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=name,
+                        measurement_unit=measurement_unit
+                    )
+
+                    if created:
+                        count_created += 1
+
+                logger.info(f'Добавлено {count_created} ингредиентов из'
+                            'файла {csv_file_path}')
+                print(f'Добавлено {count_created} ингредиентов из'
+                      'файла {csv_file_path}')
+        except FileNotFoundError as e:
+            logger.error(str(e))
