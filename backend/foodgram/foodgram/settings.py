c4afbb99d12b72ac9784634a9b8f0f4d5157e4d4
Ян Свиридов <lansviridovlansviridov@yandex.ru>
2023-12-20 23:25:38 +0300

    на ревью

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -76,15 +76,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
